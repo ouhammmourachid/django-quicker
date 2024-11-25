86a2ff5213322b20cb6826f3bfaa05e7ffa1175d
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-11-25 12:30:46 +0000

    refactor(settings): streamline settings management and remove unused utilities

--- core/project/settings/__init__.py
+++ core/project/settings/__init__.py
@@ -1,25 +1,16 @@
-import os.path
 from pathlib import Path
 
-from split_settings.tools import include, optional
+from split_settings.tools import include
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 # your project NAME
 ENVVAR_SETTINGS_PREFIX: str = "APP_NAME_SETTINGS_"
 
-LOCAL_SETTINGS_PATH = os.getenv(f"{ENVVAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH")
-
-if not LOCAL_SETTINGS_PATH:
-    LOCAL_SETTINGS_PATH = "local/settings.dev.py"
-
-if not os.path.isabs(LOCAL_SETTINGS_PATH):
-    LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
 
 include(
     "base.py",
     "custom.py",
-    optional(LOCAL_SETTINGS_PATH),
     "envvars.py",
     "docker.py",
 )
--- core/project/settings/envvars.py
+++ core/project/settings/envvars.py
@@ -1,4 +1,6 @@
-from core.general.utils.collections import deep_update
-from core.general.utils.settings import get_settings_from_environment
+from dj_split_settings.utils.collections import deep_update
+from dj_split_settings.utils.settings import get_settings_from_environment
+
+# Load settings from environment variables
 
 deep_update(globals(), get_settings_from_environment(ENVVAR_SETTINGS_PREFIX))  # type: ignore # noqa: F821
